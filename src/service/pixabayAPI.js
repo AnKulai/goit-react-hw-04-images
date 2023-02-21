84e6b602604e538c3b6aa1cc5a0c4cfd4b471fbc
AlekasndrHavryliuk <116018047+AnKulai@users.noreply.github.com>
2023-02-21 19:36:45 +0200

    Update pixabayAPI.js

--- src/service/pixabayAPI.js
+++ src/service/pixabayAPI.js
@@ -6,9 +6,11 @@ let lastQuery = '';
 const requestImages = async (query, page = 1) => {
   try {
     lastQuery = query ?? lastQuery;
-    const { data } = await axios.get(`${config.BASE_URL}`, {
+    // const { data } = await axios.get(`${config.BASE_URL}`, {
+    const { data } = await axios.get(`https://pixabay.com/api/`, {
       params: {
-        key: config.API_KEY,
+        // key: config.API_KEY,
+        key: `32974638-4592caedf5e0332eeb1bc81de`,
         q: lastQuery,
         image_type: `photo`,
         orientation: `horizontal`,
