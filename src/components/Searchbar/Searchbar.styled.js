1a1d82f61ed75637828f6fb387a567fd7148a6d5
AlekasndrHavryliuk <116018047+AnKulai@users.noreply.github.com>
2023-02-21 20:05:52 +0200

    Update Searchbar.styled.js

--- src/components/Searchbar/Searchbar.styled.js
+++ src/components/Searchbar/Searchbar.styled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
-const randomColor = () =>
-  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// const randomColor = () =>
+//   `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
 export const CustomHeader = styled.div`
   width: 100%;
-  background-color: ${randomColor()};
+  /* background-color: }; */
+  background-color: red;
   display: flex;
   align-items: center;
   justify-content: center;
